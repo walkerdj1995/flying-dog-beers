49aee875b595b223526c4bbd64457726d372f9fc
walkerdj1995 <46785761+walkerdj1995@users.noreply.github.com>
2019-09-30 17:21:36 +0100

    testing removals 2

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table as dt
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+#import dash_table as dt
+#import dash_bootstrap_components as dbc
+#from dash.dependencies import Input, Output, State
 #import plotly.express as px
 #import plotly.graph_objects as go
 #from dash.exceptions import PreventUpdate
