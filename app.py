ba83fc2da2a85e1e532d0029c3b0f6b10df31fa7
walkerdj1995 <46785761+walkerdj1995@users.noreply.github.com>
2019-09-30 17:47:49 +0100

    c

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table as dt
-#import dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc
 #from dash.dependencies import Input, Output, State
 #import plotly.express as px
 import plotly.graph_objects as go
